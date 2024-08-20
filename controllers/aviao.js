7e1447e3e6d626e4d14d873ef15c07c92f71bea1
Luanhts <luanh3nrique08@gmail.com>
2024-08-20 20:42:11 -0300

    feat: aviao

---
+++ controllers/aviao.js
@@ -0,0 +1,54 @@
+const db = [];
+
+let nextId = 1;
+
+const model = (id = nextId++) => {
+    if(body.airPlane != "" && body.airPlane != undefined &&){
+        return{
+            id,
+            airPlane: body.name,
+            //empresa: body.empresa
+        };
+    };
+};
+
+const store = body => {
+    const novo = model(body);
+
+    if(novo){
+        db.push(novo);
+
+        return 201;
+    }
+
+    return 400;
+}
+
+const index = () => {
+    return db;
+}
+
+const show = id => {
+    db.find(el => el.id == id);
+}
+
+const update = (id, body) => {
+    const index = db.findIndex(el => el.id == id);
+
+    const novo = model(body, parseInt(id));
+
+    if(index != -1 && db != "" && novo) { // pode dar erro no db ""
+        db[index] = novo;
+        return 200;
+    }
+}
+
+const destroy = (id) => {
+    const index = db.findIndex((el) => el.id == id);
+
+    if(index != -1){
+        db.splice(index, 1);
+        return 200;
+    };
+    return 404;
+};
